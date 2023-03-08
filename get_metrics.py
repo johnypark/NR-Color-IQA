fa51c161eaf0e22a9b775fddb07308daa1b66f42
johnypark <parkjohnyc@gmail.com>
2023-03-07 20:10:12 -0500

    Create get_metrics.py

---
+++ get_metrics.py
@@ -0,0 +1,43 @@
+#!python
+
+import pandas as pd
+import tensorflow as tf
+import os
+import sys
+import time
+import glob
+import NR_Color_IQA as iqa
+
+QICI_colorful = []
+BRISQUE = []
+UCIQE = []
+img_filename = []
+glob_PATH = sys.argv[1]
+ls_all_files = glob.glob(glob_PATH)
+RESIZE_RESOL = 512
+
+def main():
+    
+    QICI_colorful = []
+    BRISQUE = []
+    UCIQE = []
+    img_fileName = []
+    for file_name in ls_all_files:
+        start = time.time()
+        print('Reading file from PATH:{}'.format(file_name))        
+        tens = tf.keras.utils.load_img(file_name)
+        tens = tf.image.resize(tens, (RESIZE_RESOL, RESIZE_RESOL))
+        img_filename += [file_name.split("/")[-1]]
+        QICI_colorful += [iqa.get_colorfulness()(tens).numpy()]
+        BRISQUE += [iqa.brisque(tens.numpy()).tolist()] 
+        UCIQE += [iqa.get_uciqe(tens.numpy())]
+        end = time.time()
+        print("Time to run: {}".format((end - start)))
+    
+df_part1 = pd.DataFrame({'file_name':img_filename, 'QICI_colorful': QICI_colorful, 
+              'UCIQE':UCIQE})
+df_part2 = pd.DataFrame(BRISQUE, columns = iqa.get_brisque_colnames(['brisque']))
+pd.concat([df_part1, df_part2], axis = 1).to_csv('get_metrics_out.csv')
+
+if __name__ == '__main__':
+    main()
